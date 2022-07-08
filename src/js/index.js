a1b0d0f9eb60ed27e5667174b3b81d7129611c85
David Acosta <davidacosta.dev@gmail.com>
2022-07-08 15:26:29 -0700

    implemented rolling and pacing animation

---
+++ src/js/index.js
@@ -0,0 +1,50 @@
+const state = {
+  isDay: true,
+  petName: "",
+  isHatched: false,
+  getsHungry: null,
+  getsBored: null,
+  getsSleepy: null,
+  dino: null,
+  chaoGif: null,
+};
+
+let chaoAnim = document.getElementById("chao");
+
+class Pet {
+  constructor(name) {
+    this.name = name;
+    this.age = 0;
+    this.hunger = 0;
+    this.boredom = 0;
+    this.sleepiness = 0;
+  }
+
+  feedMe = () => {
+    console.log("I'm HUNGERY!!! Feed me!!");
+  };
+
+  hatch = () => {
+    chaoAnim.style.backgroundImage =
+      "url(./resources/images/hatching-animation-SLOW.gif)";
+    console.log(this.name + " has hatched!");
+    state.isHatched = true;
+    setTimeout(() => {
+      chaoAnim.classList.remove("egg");
+      chaoAnim.classList.add("idle");
+      chaoAnim.style.backgroundImage = "url(./resources/images/chao_idle.gif)";
+    }, 6000);
+  };
+} //end of class
+
+const startButton = document.getElementById("start-button");
+
+const startGame = () => {
+  const namePrompt = prompt("Hi, please enter your pet's name: ");
+  state.dino = new Pet(namePrompt);
+  console.log(`${state.dino.name} has been instantiated`);
+  console.log("incubation has started...");
+  setTimeout(state.dino.hatch, 2000); //FOR DEBUGGING************
+};
+
+startButton.addEventListener("click", startGame);
